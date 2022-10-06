b47596ec2b7b4d4f36d8c755a02964562a29150c
szymon <DESKTOP-C0TU3RB>
2022-10-06 22:13:48 +0200

    Created At Route

--- Apartments_API/Controllers/ApartmentAPIController.cs
+++ Apartments_API/Controllers/ApartmentAPIController.cs
@@ -10,12 +10,16 @@ namespace Apartments_API.Controllers
     public class ApartmentAPIController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult <IEnumerable<ApartmentDTO>> GetApartments()
         {
             return Ok(ApartmentStore.apartmentList);
             
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetApartment")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult <ApartmentDTO> GetApartment(int id)
         {
             if (id == 0)
@@ -29,6 +33,23 @@ namespace Apartments_API.Controllers
             }
             return Ok(apartment);
 
+        }
+
+        [HttpPost]
+        public ActionResult<ApartmentDTO> CreateApartment(ApartmentDTO apartmentDTO)
+        {
+            if (apartmentDTO == null)
+            {
+                return BadRequest(apartmentDTO);
+            }
+            if (apartmentDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            apartmentDTO.Id = ApartmentStore.apartmentList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            ApartmentStore.apartmentList.Add(apartmentDTO); //we added this villa object to Store
+
+            return CreatedAtRoute("GetApartment", new { id = apartmentDTO.Id },apartmentDTO);
         }
     }
 }
