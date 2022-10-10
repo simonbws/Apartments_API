ea34b496c206b2cd204b1e3a21fa75efe5718a3e
szymon <DESKTOP-C0TU3RB>
2022-10-10 23:23:00 +0200

    Foreign Key added to ApartmentNumber table related to Apartment table

---
+++ Apartments_API/Migrations/20221010211703_AddForeignKeyToApartmentTable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Apartment_API.Migrations
+{
+    public partial class AddForeignKeyToApartmentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApartmentID",
+                table: "ApartmentNumbers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 23, 17, 3, 685, DateTimeKind.Local).AddTicks(1789));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 23, 17, 3, 685, DateTimeKind.Local).AddTicks(1821));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 23, 17, 3, 685, DateTimeKind.Local).AddTicks(1824));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 23, 17, 3, 685, DateTimeKind.Local).AddTicks(1826));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 23, 17, 3, 685, DateTimeKind.Local).AddTicks(1828));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApartmentNumbers_ApartmentID",
+                table: "ApartmentNumbers",
+                column: "ApartmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApartmentNumbers_Apartments_ApartmentID",
+                table: "ApartmentNumbers",
+                column: "ApartmentID",
+                principalTable: "Apartments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApartmentNumbers_Apartments_ApartmentID",
+                table: "ApartmentNumbers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApartmentNumbers_ApartmentID",
+                table: "ApartmentNumbers");
+
+            migrationBuilder.DropColumn(
+                name: "ApartmentID",
+                table: "ApartmentNumbers");
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 15, 19, 42, 665, DateTimeKind.Local).AddTicks(7472));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 15, 19, 42, 665, DateTimeKind.Local).AddTicks(7507));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 15, 19, 42, 665, DateTimeKind.Local).AddTicks(7509));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 15, 19, 42, 665, DateTimeKind.Local).AddTicks(7511));
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2022, 10, 10, 15, 19, 42, 665, DateTimeKind.Local).AddTicks(7512));
+        }
+    }
+}
--- Apartments_API/Models/ApartmentNumber.cs
+++ Apartments_API/Models/ApartmentNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Apartments_API.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Apartment_API.Models
@@ -7,6 +8,9 @@ namespace Apartment_API.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)] // Apartment number must be provided by the User
         public int ApartmentNo { get; set; }
+        [ForeignKey("Apartment")]
+        public int ApartmentID { get; set; }
+        public Apartment Apartment { get; set; }
         public string SpecialProperties{ get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
