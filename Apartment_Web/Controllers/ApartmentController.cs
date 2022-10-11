fe31abfff0d65bfa9f32b3edf1a65249e1206b52
szymon <DESKTOP-C0TU3RB>
2022-10-11 20:04:52 +0200

    Index Apartment View added with first UI

---
+++ Apartment_Web/Controllers/ApartmentController.cs
@@ -0,0 +1,35 @@
+﻿using Apartment_Web.Models;
+using Apartment_Web.Models.DTO;
+using Apartment_Web.Services.IServices;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Apartment_Web.Controllers
+{
+    public class ApartmentController : Controller
+    {
+        private readonly IApartmentService _apartmentService;
+        private readonly IMapper _mapper;
+
+        public ApartmentController(IApartmentService apartmentService, IMapper mapper)
+        {
+            _apartmentService = apartmentService;
+            _mapper = mapper;
+        }
+ 
+        public async Task<IActionResult> IndexApartment()
+        {
+            List<ApartmentDTO> list = new();
+
+            var response = await _apartmentService.GetAllAsync<APIResponse>();
+            if (response != null && response.isSuccess)
+            {
+                //we convert that to string, then we deserialize that to list od apart dto and we assign that to list
+                list = JsonConvert.DeserializeObject<List<ApartmentDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+            
+        }
+    }
+}
