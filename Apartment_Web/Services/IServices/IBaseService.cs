ff5bc4625c90b350e0c4eb4626a5d1e5acab2d88
szymon <DESKTOP-C0TU3RB>
2022-10-11 15:44:42 +0200

    Base Service for API

---
+++ Apartment_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Apartment_Web.Models;
+
+namespace Apartment_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest aPIRequest); // returned type will be Type<T> generic
+    }
+}
