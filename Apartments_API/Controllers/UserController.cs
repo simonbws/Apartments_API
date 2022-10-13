461e4f444b359e0c56872d2f656ce4e8ad44013e
szymon <DESKTOP-C0TU3RB>
2022-10-14 01:04:26 +0200

    User Controller(Login and Register Action Methods)

---
+++ Apartments_API/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Apartment_API.Models;
+using Apartment_API.Models.DTO;
+using Apartment_API.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Apartment_API.Controllers
+{
+    [Route("api/UserAuth")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        protected APIResponse _response;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            this._response = new(); // we initialize the response
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
+        {
+            var loginResponse = await _userRepository.Login(model);
+            if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.isSuccess = false;
+                _response.Errors.Add("Username or password is invalid");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.isSuccess = true;
+            _response.Result = loginResponse;
+            return Ok(_response);
+            
+
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
+        {
+            bool ifUserIsUnique = _userRepository.isUniqueUser(model.UserName);
+            if (!ifUserIsUnique)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.isSuccess = false;
+                _response.Errors.Add("Sorry, UserName already exists!");
+                return BadRequest(_response);
+            }
+            var user = await _userRepository.Register(model);
+            if (user == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.isSuccess = false;
+                _response.Errors.Add("Error while register");
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+    }
+}
--- Apartments_API/Repository/UserRepository.cs
+++ Apartments_API/Repository/UserRepository.cs
@@ -2,16 +2,23 @@
 using Apartment_API.Models;
 using Apartment_API.Models.DTO;
 using Apartment_API.Repository.IRepository;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Apartment_API.Repository
 {
     public class UserRepository : IUserRepository
     {
         private readonly AppDbContext _db;
-
-        public UserRepository(AppDbContext db)
+        private string privateKey;
+        public UserRepository(AppDbContext db, IConfiguration configuration)
         {
-            
+            _db = db;
+            privateKey = configuration.GetValue<string>("ApiSettings:Secret");
+
         }
 
         public bool isUniqueUser(string username)
@@ -24,9 +31,41 @@ namespace Apartment_API.Repository
             return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequest)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+            && u.Password == loginRequestDTO.Password);
+            //if user is null or invalid that means that user is not found
+            if (user == null)
+            {
+                return new LoginResponseDTO()
+                {
+                    Token = "",
+                    User = null
+                };
+            }
+            //if user is found we need to generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(privateKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            //once we defined all the properties, we need to generate token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+            return loginResponseDTO;
         }
 
         public async Task<LocalUser> Register(RegisterRequestDTO registerRequestDTO)
